cb8156b281680227974969cce90feaa88a7788e9
DHARAKA DM <deshandm699@gmail.com>
2024-05-06 19:11:26 +0530

    nav bar for responsive

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,39 +1,47 @@
+"use client"
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import NavLinks from './navLinks/NavLinks';
 import style from './links.module.css'
 
 const Links = () => {
 
-    const links = [
-        {
-          title: "Homepage",
-          path: "/",
-        },
-        {
-          title: "About",
-          path: "/about",
-        },
-        {
-          title: "Contact",
-          path: "/contact",
-        },
-        {
-          title: "Blog",
-          path: "/blog",
-        },
-      ];
-const session=true;
-const admin=true;
+  const [open, setoOpen] = useState(false);
+
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+  const session = true;
+  const admin = true;
   return (
     <div className={style.links}>
-      {links.map(({title,path})=><NavLinks title={title} path={path} key={title}/>)}
-      {session ?(
+      {links.map(({ title, path }) => <NavLinks title={title} path={path} key={title} />)}
+      {session ? (
         <>
-        {admin && <NavLinks title={'Admin'} path={'/admin'}/>}
-        <button className={style.logout}>LogOut</button>
+          {admin && <NavLinks title={'Admin'} path={'/admin'} />}
+          <button className={style.logout}>LogOut</button>
         </>
-      ):<NavLinks title={'Login'} path={'/login'}/>}
+      ) : <NavLinks title={'Login'} path={'/login'} />}
+
+      <button onClick={(() => setoOpen((prev)=>!prev))}>Menu</button>
+      {
+        open && <div>{links.map(({ title, path }) => <NavLinks title={title} path={path} key={title} />)}</div>
+      }
     </div>
   )
 }
