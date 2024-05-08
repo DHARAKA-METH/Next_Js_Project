ec3d9e3d7220f1beb7b93f64a38983c4b3876a12
DHARAKA DM <deshandm699@gmail.com>
2024-05-08 16:37:14 +0530

    next js Image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
   return (
     <div>
-    About
+    <Image src={"/about.png"} width={500} height={500} alt=''/>
     </div>
   )
 }
