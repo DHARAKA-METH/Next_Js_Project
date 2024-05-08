294dfbaa749a71e895555155ad24a2a1a6e44903
DHARAKA DM <deshandm699@gmail.com>
2024-05-08 16:51:59 +0530

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Links from './links/Links'
 import style from './Navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div>
     <div className={style.container}>
-      <div className={style.logo}>Logo</div>
+    <Link href="/"><div className={style.logo}>Logo</div></Link>
       <div >
         <Links/>
         </div>
