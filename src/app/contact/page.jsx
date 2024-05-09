0ec9cb49623b3bd419c385432e1911a64b2bd415
DHARAKA DM <deshandm699@gmail.com>
2024-05-09 10:37:00 +0530

    contact page structure

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Image from 'next/image';
+import styles from './contact.module.css';
 
 const Contact = () => {
   return (
-    <div>
-    Contact
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" width={500} height={500} className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        {/* <HydrationTestNoSSR/> */}
+        {/* <div suppressHydrationWarning>{a}</div> */}
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   )
 }
