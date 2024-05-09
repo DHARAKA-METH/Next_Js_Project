7ac3b4779e943f16fafa1d6a2f4d9a049f37783c
DHARAKA DM <deshandm699@gmail.com>
2024-05-09 14:33:51 +0530

    post page design update

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "./singlePost.module.css";
 
-const BlogTitle = () => {
+
+
+const SinglePostPage = () => {
+
   return (
-    <div>
-    BlogTitle
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="/post.png" width={400} height={700} alt="" />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image className={styles.avatar} width={50} height={50} src="/post.png"  alt="" />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Jefferson</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          burn piece fat by charge date love experiment move beneath teeth seed compound valley clear organization dream bell mouse breathe frog heavy tax highest
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogTitle
+export default SinglePostPage;
--- src/components/postCard/postCard.js
+++ src/components/postCard/postCard.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
 
+
 const PostCard = () => {
     return (
         <div className={styles.container}>
@@ -13,8 +14,8 @@ const PostCard = () => {
             </div>
             <div className={styles.bottom}>
                 <h1 className={styles.title}>Title</h1>
-                <p className={styles.desc}>Desc</p>
-                <Link href="/blog/post">READ MORE</Link>
+                <p className={styles.desc}>that father outside principle perhaps automobile situation possibly bring enter stretch far strong bear poetry tall should pet breeze appearance task barn ten writing</p>
+                <Link className={styles.link} href="/blog/post">READ MORE</Link>
             </div>
         </div>
     )
