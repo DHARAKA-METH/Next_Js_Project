b0c9b8465f1333a619feb7f6ba2401c791320977
DHARAKA DM <deshandm699@gmail.com>
2024-05-12 18:59:42 +0530

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,14 @@
-import styles from './footer.module.css';
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>lamadev</div>
-      <div className={styles.text}>Lama creative thoughts agency © All rights reserved.</div>
-      
+      <div className={styles.text}>
+        Lama creative thoughts agency © All rights reserved.
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
