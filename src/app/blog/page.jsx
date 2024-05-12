186c50ee7b11152179f65fe861536d0d1181ee14
DHARAKA DM <deshandm699@gmail.com>
2024-05-12 11:49:44 +0530

    update

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,23 +1,22 @@
-import PostCard from "@/components/postCard/postCard"
-import styles from "./blog.module.css"
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
 const BlogPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.post}>
- <PostCard/>
+        <PostCard />
       </div>
       <div className={styles.post}>
- <PostCard/>
+        <PostCard />
       </div>
       <div className={styles.post}>
- <PostCard/>
+        <PostCard />
       </div>
       <div className={styles.post}>
- <PostCard/>
+        <PostCard />
       </div>
- 
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
