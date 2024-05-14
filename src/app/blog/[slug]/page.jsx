981b06c23c4d350d9f70e455ac1af5f2edc6963e
DHARAKA DM <deshandm699@gmail.com>
2024-05-14 12:38:04 +0530

    single post page data get by API

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,19 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+  if (!res.ok) {
+    throw new Error("Something Went Wrong");
+  } else {
+    return res.json();
+  }
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+  const post = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
@@ -14,7 +26,7 @@ const SinglePostPage = () => {
         />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image
             className={styles.avatar}
@@ -32,11 +44,7 @@ const SinglePostPage = () => {
             <span className={styles.detailValue}>01.01.2024</span>
           </div>
         </div>
-        <div className={styles.content}>
-          burn piece fat by charge date love experiment move beneath teeth seed
-          compound valley clear organization dream bell mouse breathe frog heavy
-          tax highest
-        </div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
