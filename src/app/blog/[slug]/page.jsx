67c375115a1425c525683af3d700da77a39c993f
DHARAKA DM <deshandm699@gmail.com>
2024-05-14 10:45:19 +0530

    update

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,20 +1,28 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
 
-
-
-const SinglePostPage = ({params}) => {
-console.log('hear params',params)
-  
-return (
+const SinglePostPage = () => {
+  return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image className={styles.img} src="/post.png" width={400} height={700} alt="" />
+        <Image
+          className={styles.img}
+          src="/post.png"
+          width={400}
+          height={700}
+          alt=""
+        />
       </div>
       <div className={styles.textContainer}>
         <h1 className={styles.title}>Title</h1>
         <div className={styles.detail}>
-          <Image className={styles.avatar} width={50} height={50} src="/post.png" alt="" />
+          <Image
+            className={styles.avatar}
+            width={50}
+            height={50}
+            src="/post.png"
+            alt=""
+          />
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Author</span>
             <span className={styles.detailValue}>Terry Jefferson</span>
@@ -25,7 +33,9 @@ return (
           </div>
         </div>
         <div className={styles.content}>
-          burn piece fat by charge date love experiment move beneath teeth seed compound valley clear organization dream bell mouse breathe frog heavy tax highest
+          burn piece fat by charge date love experiment move beneath teeth seed
+          compound valley clear organization dream bell mouse breathe frog heavy
+          tax highest
         </div>
       </div>
     </div>
