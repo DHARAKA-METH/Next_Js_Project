17a8ee83c8401d0f21549b18ecc0a5a3475fa925
DHARAKA DM <deshandm699@gmail.com>
2024-05-14 11:00:45 +0530

    fetch data API

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,20 +1,27 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
-const BlogPage = () => {
+
+
+
+const  getData=async ()=>{
+  const res=await fetch("https://jsonplaceholder.typicode.com/posts")
+  if(!res.ok){
+    throw new Error('Something Went Wrong')
+  }else{
+    return res.json()
+  }
+}
+
+
+const BlogPage = async () => {
+  const posts = await getData()
   return (
     <div className={styles.container}>
-      <div className={styles.posts}>
-        <PostCard />
-      </div>
-      <div className={styles.posts}>
-        <PostCard />
-      </div>
-      <div className={styles.posts}>
-        <PostCard />
-      </div>
-      <div className={styles.posts}>
-        <PostCard />
-      </div>
+
+      {posts.map((post)=><div className={styles.posts} key={post.id}>
+        <PostCard post={post}/>
+      </div>)}
+
     </div>
   );
 };
--- src/components/postCard/postCard.js
+++ src/components/postCard/postCard.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
 
-const PostCard = () => {
+const PostCard = ({post}) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -13,11 +13,9 @@ const PostCard = () => {
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <p className={styles.desc}>
-          that father outside principle perhaps automobile situation possibly
-          bring enter stretch far strong bear poetry tall should pet breeze
-          appearance task barn ten writing
+          {post.body}
         </p>
         <Link className={styles.link} href="/blog/post">
           READ MORE
