9b372fe80660ea0c2e65cdfd324def01ed9e2b51
DHARAKA DM <deshandm699@gmail.com>
2024-05-14 19:51:10 +0530

    create temport data

---
+++ src/lib/data.js
@@ -0,0 +1,26 @@
+//TEMPARY DATA
+
+const users=[
+    {id:1,name:'john'},
+    {id:2,name:'jane'},
+]
+
+const posts=[
+    {id:1,title:'post 1',body:'body desc.....',userId:1},
+    {id:2,title:'post 1',body:'body desc.....',userId:1},
+    {id:3,title:'post 1',body:'body desc.....',userId:2},
+    {id:4,title:'post 1',body:'body desc.....',userId:2},
+]
+
+export const getPosts= async ()=>{
+return posts
+}
+
+export const getPost=async (id)=>{
+posts.find((post)=>post.id===parseInt(id)
+}
+
+export const getUser=async (id)=>{
+posts.find((user)=>user.id===parseInt(id)
+}
+
