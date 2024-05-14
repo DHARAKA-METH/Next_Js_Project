8b35447195d8ad7c7e7f42a1497560d32969f7d5
DHARAKA DM <deshandm699@gmail.com>
2024-05-14 19:55:50 +0530

    update blog post from tempory data

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,19 +1,23 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
+import { getPosts } from "@/lib/data";
 
-const getData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: { revalidate: 3600 },
-  });
-  if (!res.ok) {
-    throw new Error("Something Went Wrong");
-  } else {
-    return res.json();
-  }
-};
+// const getData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//     next: { revalidate: 3600 },
+//   });
+//   if (!res.ok) {
+//     throw new Error("Something Went Wrong");
+//   } else {
+//     return res.json();
+//   }
+// };
 
 const BlogPage = async () => {
-  const posts = await getData();
+  // const posts = await getData();
+
+  //get post data from tempory lib
+  const posts = await getPosts();
   return (
     <div className={styles.container}>
       {posts.map((post) => (
