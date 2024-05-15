5c73fe57900728b8750a4351765556a0d324c79c
DHARAKA DM <deshandm699@gmail.com>
2024-05-15 22:24:53 +0530

    create mongodb connection

---
+++ src/lib/utils.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+export const connectToDb = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log("use exisiting connection");
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
