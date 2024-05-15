5cb38f2faddde8d81d548bb7e5843800ac634d11
DHARAKA DM <deshandm699@gmail.com>
2024-05-15 15:34:06 +0530

    update pictures with online link

--- src/components/postCard/postCard.js
+++ src/components/postCard/postCard.js
@@ -8,7 +8,7 @@ const PostCard = ({ post }) => {
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-          <Image src="/post.png" alt="" fill className={styles.img} />
+          <Image src="https://images.pexels.com/photos/22873741/pexels-photo-22873741/free-photo-of-close-up-of-a-cup-of-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
         </div>
         <span className={styles.date}>01.01.2024</span>
       </div>
