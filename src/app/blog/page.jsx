d2e44e424b9f7cb507e5360e508b89df68b69379
DHARAKA DM <deshandm699@gmail.com>
2024-05-18 19:48:16 +0530

    find post by server (Api route)

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,28 +2,28 @@ import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
 import { getPosts } from "@/lib/data";
 
-
 export const metadata = {
   title: "Blog Page",
   description: "Blog description",
-}
+};
 
-// const getData = async () => {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-//     next: { revalidate: 3600 },
-//   });
-//   if (!res.ok) {
-//     throw new Error("Something Went Wrong");
-//   } else {
-//     return res.json();
-//   }
-// };
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/blog", {
+    next: { revalidate: 3600 },
+  });
+  if (!res.ok) {
+    throw new Error("Something Went Wrong");
+  } else {
+    return res.json();
+  }
+};
 
 const BlogPage = async () => {
-  // const posts = await getData();
+  // Fetch by API
+  const posts = await getData();
 
   //get post data from tempory lib
-  const posts = await getPosts();
+  // const posts = await getPosts();
   return (
     <div className={styles.container}>
       {posts.map((post) => (
