629b01e633bee9cf629892d4ed4aec105a5e3a34
DHARAKA DM <deshandm699@gmail.com>
2024-05-18 19:10:25 +0530

    saved to new post to mongodb database

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,5 +1,28 @@
 "use server";
+
+import { revalidatePath } from "next/cache";
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
 export const formData = async (formData) => {
   const { title, desc, img, userId, slug } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+    const newPost = new Post({
+      title,
+      desc,
+      img,
+      userId,
+      slug,
+    });
+    await newPost.save();
+    console.log("saved to db");
+    revalidatePath("/blog"); // for refresh page
+  } catch (err) {
+    console.log(err);
+    return { error: "somthing error" };
+  }
+
   console.log(title, desc, img, userId, slug);
 };
