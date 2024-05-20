7209489aa57cccc20f38642f83d2c3c597cdad24
DHARAKA DM <deshandm699@gmail.com>
2024-05-20 15:20:39 +0530

    login page design

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,10 +1,23 @@
 import { loginHandle } from "@/lib/action";
+import styles from "./loginForm.module.css";
 const LoginPage = async () => {
   return (
     <div>
       <form action={loginHandle}>
         <button>Login with Github</button>
       </form>
+
+
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <form className={styles.form} action={loginForm}>
+            <input type="text" placeholder="username" name="username" />
+            {/* <input type="email" placeholder="email" name="email" /> */}
+            <input type="password" placeholder="password" name="password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
