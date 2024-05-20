bef5fff8c7b1784f3305bfb0e119434daffb846f
DHARAKA DM <deshandm699@gmail.com>
2024-05-20 13:30:24 +0530

    registerPage design

---
+++ src/app/register/page.js
@@ -0,0 +1,24 @@
+"use client";
+import styles from "./registerForm.module.css";
+
+const RegisterForm = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form className={styles.form} action="">
+          <input type="text" placeholder="username" name="username" />
+          <input type="email" placeholder="email" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <input
+            type="password"
+            placeholder="password again"
+            name="passwordRepeat"
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterForm;
