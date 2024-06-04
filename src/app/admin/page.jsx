149720df006650370838b801f5acd8b00388e94a
DHARAKA DM <deshandm699@gmail.com>
2024-06-04 18:52:16 +0530

    delete post

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,16 @@
+import AdminPostForm from "@/components/adminPostForm/AdminPostForm";
 import React from "react";
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
-};
+  return( <div>
+    AdminPage
+    <div>
+      <AdminPostForm/>
+    </div>
+    <div>
+    
+    </div>
+    </div>
+)};
 
 export default AdminPage;
--- src/components/adminPostForm/AdminPostForm.js
+++ src/components/adminPostForm/AdminPostForm.js
@@ -1,11 +1,31 @@
-import React from 'react'
 
-const adminPostForm = () => {
+import { getPosts } from "@/lib/data";
+import styles from "./adminPostForm.module.css";
+import Image from "next/image";
+import { deletePost } from "@/lib/action";
+
+const AdminPostForm = async () => {
+  const posts = await getPosts();
+
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <h2>Posts</h2>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Image alt={post.id} src={post.img} height={50} width={50} />
+          <p>{post.desc}</p>
+          <div>
+            <form action={deletePost}>
+              <input type="hidden" name="userId" value={post.id}/>
+              <button>Delete</button>
 
-export default adminPostForm
+            </form>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AdminPostForm;
