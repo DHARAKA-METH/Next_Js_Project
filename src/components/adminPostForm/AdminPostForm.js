870109492214d22c2f0fa427311776b952a8f6aa
DHARAKA DM <deshandm699@gmail.com>
2024-06-05 21:40:08 +0530

    admin post form style by css

--- src/components/adminPostForm/AdminPostForm.js
+++ src/components/adminPostForm/AdminPostForm.js
@@ -1,21 +1,24 @@
 "use client";
 import { newPost } from "@/lib/action";
 import { useFormState } from "react-dom";
+import styles from "./adminPostForm.module.css"
 
 const AdminPostForm = ({ userId }) => {
-    const [state, formAction] = useFormState(newPost, undefined);
-    return (
-        <form action={formAction}>
-            <h1>ADD New Post</h1>
-            <input type="text" name="title" placeholder="Title" />
-            <input type="text" name="slug" placeholder="Slug" />
-            <input type="hidden" name="userId" value={userId} />
-            <input type="text" name="img" placeholder="img" />
-            <textarea type="text" name="desc" placeholder="desc" rows={10} />
-            <button>Add</button>
-            {state && state.error}
-        </form>
-    );
+  const [state, formAction] = useFormState(newPost, undefined);
+  return (
+    <div className={styles.container}>
+      <h2>ADD New Post</h2>
+      <form className={styles.form} action={formAction}>
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="slug" placeholder="Slug" />
+        <input type="hidden" name="userId" value={userId} />
+        <input type="text" name="img" placeholder="img" />
+        <textarea type="text" name="desc" placeholder="desc" rows={10} />
+        <button>Add</button>
+        {state && state.error}
+      </form>
+    </div>
+  );
 };
 
 export default AdminPostForm;
